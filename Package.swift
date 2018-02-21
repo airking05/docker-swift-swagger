51eb5a133511f22e00be9abc1cffd30099151d77
physalis02 <yosuke.otosu@gmail.com>
2018-02-21 21:39:39 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "DockerSwiftSwagger",
+    name: "docker-swift-swagger",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "DockerSwiftSwagger",
-            targets: ["DockerSwiftSwagger"]),
+            name: "docker-swift-swagger",
+            targets: ["docker-swift-swagger"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "DockerSwiftSwagger",
+            name: "docker-swift-swagger",
             dependencies: []),
         .testTarget(
-            name: "DockerSwiftSwaggerTests",
-            dependencies: ["DockerSwiftSwagger"]),
+            name: "docker-swift-swaggerTests",
+            dependencies: ["docker-swift-swagger"]),
     ]
 )
